4c19e7bb15bd92475bf045462de771ee2f59dcb4
Qwertbun <81864346+Qwertbun@users.noreply.github.com>
2024-05-11 15:47:10 +0300

    upgrade

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,4 @@
 class Config {
-  static final Uri launcherJarUrl =
-      Uri.parse('http://owa.mine-souls.ru:9274/MineSoulsLauncher.jar');
-
-  static final String projectDirectoryName = 'MineSoulsRp-Dart';
+  static final Uri launcherJarUrl = Uri.parse('http://owa.mine-souls.ru:9274/MineSoulsLauncher.jar');
+  static final String projectDirectoryName = 'MineSoulsRp';
 }
